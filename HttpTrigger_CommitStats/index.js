0d7c2427924f2f057fac9ae51c7d3819bf0c0503
Michael Szul <michael@szul.us>
2018-03-10 18:38:25 -0500

    Fixed debugger and first GitHub call

--- HttpTrigger_CommitStats/index.js
+++ HttpTrigger_CommitStats/index.js
@@ -3,16 +3,34 @@ module.exports = function (context, req) {
     context.log("JavaScript HTTP trigger function processed a request.");
     try {
         context.log("Connecting to GitHub...");
-        
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
+        const uname = req.query.name;
+        octokit.repos.getForUser({
+            username: uname
+            , type: "all"
+            , sort: "pushed"
+            , direction: "desc"
+            , page: 1
+            , per_page: 100
+        }).then(result => {
+            const data = result.data;
+            let g = data.map(x => {
+                return { id: x.id, name: x.name, isFork: x.fork }
+            });
+            context.res = {
+                 status: 200
+                 , body: "Processed..." //Build response JSON
+            };
+        }).catch(err => {
+            context.res = {
+                status: 500
+                , body: `Any error occurred while getting the repository list: ${e}`
+            };
+        });
     }
     catch(e) {
         context.res = {
-            status: 500,
-            body: `Any error has occurred while processing your request: ${e}`
+            status: 500
+            , body: `Any error has occurred while processing your request: ${e}`
         };
     }
     context.done();
