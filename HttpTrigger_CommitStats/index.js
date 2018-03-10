7297e42bb9cc3e69d71b9072edb93e6bee36c75b
Michael Szul <michael@szul.us>
2018-03-10 11:12:53 -0500

    Initial commit

---
+++ HttpTrigger_CommitStats/index.js
@@ -0,0 +1,21 @@
+const octokit = require('@octokit/rest')();
+
+module.exports = function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    if (req.query.name || (req.body && req.body.name)) {
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: "Hello " + (req.query.name || req.body.name)
+        };
+        const result = await octokit.repos.getAll({visibility, affiliation, type, sort, direction, page, per_page});
+        console.log(result);
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }
+    context.done();
+};
---
+++ README.md
@@ -0,0 +1,5 @@
+# Commit Hub
+
+To work with this repository, make sure you have the Azure Functions tools installed locally.
+
+    npm install -g azure-functions-core-tools
