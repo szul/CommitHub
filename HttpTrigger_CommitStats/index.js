44fc8ba3087d03611db981ccfca354aa8338b75f
Michael Szul <michael@szul.us>
2018-03-10 11:25:55 -0500

    Got it running

--- HttpTrigger_CommitStats/index.js
+++ HttpTrigger_CommitStats/index.js
@@ -1,20 +1,18 @@
-const octokit = require('@octokit/rest')();
-
 module.exports = function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    if (req.query.name || (req.body && req.body.name)) {
+    const octokit = require("@octokit/rest")();
+    context.log("JavaScript HTTP trigger function processed a request.");
+    try {
+        context.log("Connecting to GitHub...");
+        
         context.res = {
             // status: 200, /* Defaults to 200 */
             body: "Hello " + (req.query.name || req.body.name)
         };
-        const result = await octokit.repos.getAll({visibility, affiliation, type, sort, direction, page, per_page});
-        console.log(result);
     }
-    else {
+    catch(e) {
         context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
+            status: 500,
+            body: `Any error has occurred while processing your request: ${e}`
         };
     }
     context.done();
